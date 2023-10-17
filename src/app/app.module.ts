d41b089ad8615a89868eaa23666351b18fc457a8
jomarcatacutan <jose@clarkoutsourcing.com>
2023-10-17 13:23:05 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { InfoComponent } from './info/info.component';
+import { ContactComponent } from './contact/contact.component';
+import { WritingComponent } from './writing/writing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    InfoComponent,
+    ContactComponent,
+    WritingComponent
   ],
   imports: [
     BrowserModule,

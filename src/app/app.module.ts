3e2269f16a52a8aff4d075edbf09eecdc72853a9
jomarcatacutan <jose@clarkoutsourcing.com>
2023-10-17 13:29:58 +0800

    removed about me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AboutMeComponent } from './about-me/about-me.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { InfoComponent } from './info/info.component';
 import { ContactComponent } from './contact/contact.component';
@@ -14,7 +13,6 @@ import { WritingComponent } from './writing/writing.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutMeComponent,
     ProjectsComponent,
     InfoComponent,
     ContactComponent,
